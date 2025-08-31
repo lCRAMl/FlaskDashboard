e0e80d866ad37c70c2b27200573e0ea5253e0ba1
lCRAMl <rudi@simmerling.info>
2025-08-31 05:45:12 +0200

    stdout has to be pipe

--- app/stream.py
+++ app/stream.py
@@ -3,15 +3,19 @@ import os
 import subprocess
 import threading
 import time
-from .config import OUTPUT_DIR, RTSP_URL
+from . import config
 
 ffmpeg_process = None
-
+stop_thread = False
 
 def start_hls_stream():
+    """Starte FFmpeg in einem eigenen Thread ohne Auto-Restart."""
+    global ffmpeg_process, stop_thread
+    stop_thread = False
+    
+    # HLS-Ausgabeordner erstellen
+    os.makedirs(config.OUTPUT_DIR, exist_ok=True)
 
-    global ffmpeg_process
-    os.makedirs(OUTPUT_DIR, exist_ok=True)
 
     def ffmpeg_thread():
         global ffmpeg_process
@@ -20,19 +24,22 @@ def start_hls_stream():
                 "ffmpeg",
                 "-nostdin",
                 "-rtsp_transport", "tcp",
-                "-i", RTSP_URL,
+                "-i", config.RTSP_URL,
                 "-c:v", "copy",
                 "-an",
                 "-f", "hls",
                 "-hls_time", "1",
                 "-hls_list_size", "5",
                 "-hls_flags", "delete_segments+append_list+omit_endlist",
-                os.path.join(OUTPUT_DIR, "stream.m3u8")
+                os.path.join(config.OUTPUT_DIR, "stream.m3u8")
             ]
             try:
+                #with open(config.FFMPEGLOG_PATH, "a") as log_file:
                 ffmpeg_process = subprocess.Popen(
                     cmd,
-                    stdout=subprocess.DEVNULL,
+                    #stdout=log_file,            # stdout ins Log
+                    stdout=subprocess.PIPE,
+                    #stderr=subprocess.STDOUT,   # stderr ebenfalls ins Log
                     stderr=subprocess.DEVNULL,
                     bufsize=1,
                     universal_newlines=True
